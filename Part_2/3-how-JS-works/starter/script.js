e0f6f64e45f3fbe94d9ff09296c2c1d070ba8f3a
petrenkonikita112263 <bl'qyfz_rfGCekrf_t@ame>
2020-05-04 19:31:36 +0300

    finish lecture about hosting, scope and this keyword

--- Part_2/3-how-JS-works/starter/script.js
+++ Part_2/3-how-JS-works/starter/script.js
@@ -18,7 +18,7 @@ function calculateAge(year) {
 
 // fo function expression
 
-// retierment(1990);                // it's the function declartion - it's expression
+// retierment(1990);                // it's not the function declartion - it's expression
 
 var retierment = function(year) {
     console.log(65 - (2020 - year));
@@ -29,12 +29,12 @@ retierment(1990);
 // variables
 
 console.log(age);                   // undefined for variable
-var age = 18;
+var age = 18;                       // this variable stores in Global Execution Context
 console.log(age);
 
 
 function foo2() {
-    var age = 14;
+    var age = 14;                   // this variable stores in variable execution object of function context
     console.log(age);
 }
 
@@ -42,26 +42,7 @@ foo2();
 console.log(age);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////
 // Lecture: Scoping
-
-
-// First scoping example
 
 /*
 var a = 'Hello!';
@@ -78,11 +59,8 @@ function first() {
 }
 */
 
-
-
 // Example to show the differece between execution stack and scope chain
 
-/*
 var a = 'Hello!';
 first();
 
@@ -92,21 +70,56 @@ function first() {
 
     function second() {
         var c = 'Hey!';
-        third()
+        third()                 // third function was called by second - thank to scoping
     }
 }
+// also second function has access to variable a and call first function
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    // console.log(c);
+    // can't access to variable c, execution stack is different
+    // it's in different scope than second function
+
+    console.log(a + d);         // -> Hello John
 }
-*/
 
 
-
-///////////////////////////////////////
 // Lecture: The this keyword
+// this stores in execution context object
+// default - window in the browser -> function call
+// point to object that is calling the method -> method call
 
+// console.log(this);              // -> the window object
+
+// calculateAge(1995);
+// function calculateAge(year) {
+//     console.log(2020 - year);
+//     console.log(this);          // refular function call
+// }
+
+var myObject = {
+    name: 'Nobody',
+    yearOfBirth: 10,
+    calculateAge: function() {
+        console.log(this);          // refers to object that called the method
+        console.log(2020 - this.yearOfBirth);
+
+        function innerFunction() {
+            console.log(this);      // window object
+            // regular function calls happend that the window object displays
+        }
+        innerFunction();
+    }
+};
+
+// myobject call calculateAge method
+myObject.calculateAge();
+
+var newObject = {
+    name: 'Someone',
+    yearOfBirth: 23,
+};
 
 
 
