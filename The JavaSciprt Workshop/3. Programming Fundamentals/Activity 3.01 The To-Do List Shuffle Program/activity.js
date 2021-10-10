619c7d85d8e81ef7ac89b5692492f7ecfd535b9b
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-10 12:56:03 +0300

    Complete Activity 3.01.

--- The JavaSciprt Workshop/3. Programming Fundamentals/Activity 3.01 The To-Do List Shuffle Program/activity.js
+++ The JavaSciprt Workshop/3. Programming Fundamentals/Activity 3.01 The To-Do List Shuffle Program/activity.js
@@ -1,10 +1,29 @@
 // Declare and initialize the todo list array
+var todoList = [
+    "Wash Laundry",
+    "Clean Silver",
+    "Write Letters",
+    "Purchase Groceries",
+    "Retrieve Mail",
+    "Prepare Dinner"
+]
 
 // Declare and initialize variable for the todo list element
+var todoListElement = document.getElementById("todo-list")
+function replaceListItems(listElement, listOfItems) {
+    listElement.innerHTML = ""
+    for (let i = 0; i < listOfItems.length; i++) {
+        let liElement = document.createElement("li")
+        liElement.appendChild(document.createTextNode(listOfItems[i]))
+        listElement.appendChild(liElement)
+    }
+}
 
 // Declare and initialize variable for shuffle button element.
+var shuffleButtonElement = document.getElementById("shuffle-button")
 
 // Add event listener function for the shuffle button element.
+shuffleButtonElement.addEventListener("click", clickShuffleButton)
 
 // Function to replace an HTML DOM list li elements with array items.
 
@@ -39,5 +58,9 @@ function getNewShuffledArray(sourceArray) {
 }
 
 // Function to handle click events for the Shuffle button
+function clickShuffleButton() {
+    replaceListItems(todoListElement, getNewShuffledArray(todoList))
+}
 
 // Update the todo list view with initial list of items
+replaceListItems(todoListElement, todoList)
