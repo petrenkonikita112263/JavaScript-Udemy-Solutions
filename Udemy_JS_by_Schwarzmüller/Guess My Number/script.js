e07bebd97833678ca92642e9b99b1750ba080059
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-22 21:29:17 +0200

    Print in console the input number after clicking the button.

--- Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
+++ Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
@@ -1,10 +1,14 @@
 "use strict"
 
-console.log(document.querySelector(".message").textContent)
-document.querySelector(".message").textContent = "🎉 Correct Number"
-console.log(document.querySelector(".message").textContent)
+// console.log(document.querySelector(".message").textContent)
+// document.querySelector(".message").textContent = "🎉 Correct Number"
+// console.log(document.querySelector(".message").textContent)
 
-document.querySelector(".number").textContent = 13
-document.querySelector(".score").textContent = 13
+// document.querySelector(".number").textContent = 13
+// document.querySelector(".score").textContent = 13
 
-document.querySelector(".guess").value = 13
+// document.querySelector(".guess").value = 13
+
+document.querySelector(".check").addEventListener("click", function () {
+    console.log(document.querySelector(".guess").value)
+})
