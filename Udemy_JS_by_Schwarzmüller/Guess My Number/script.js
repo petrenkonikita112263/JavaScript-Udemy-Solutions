6a0dedffe8d8fd1f111d6e34b70ae715cc5b348e
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-22 21:21:41 +0200

    Set the guess number and score to some number.

--- Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
+++ Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
@@ -3,3 +3,6 @@
 console.log(document.querySelector(".message").textContent)
 document.querySelector(".message").textContent = "🎉 Correct Number"
 console.log(document.querySelector(".message").textContent)
+
+document.querySelector(".number").textContent = 13
+document.querySelector(".score").textContent = 13
