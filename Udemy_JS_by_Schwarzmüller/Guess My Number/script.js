9c9c58d612e89f6ecb400890d3cfcad637cbb55a
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-23 14:44:34 +0200

    Improve game logic comparing secretNumber with user's guessNumber.

--- Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
+++ Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
@@ -9,10 +9,32 @@
 
 // document.querySelector(".guess").value = 13
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+document.querySelector(".number").textContent = secretNumber
+var score = 20
+
 document.querySelector(".check").addEventListener("click", function () {
     let guess = Number(document.querySelector(".guess").value)
     console.log(guess, typeof guess)
     if (!guess) {
         document.querySelector(".message").textContent = "❌ No Number"
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent = "🎉 Correct Number"
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "📈 Too High!"
+            score--
+            document.querySelector(".score").textContent = score
+        } else {
+            document.querySelector(".message").textContent = "🥺 You Lose!"
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "📉 Too Low!"
+            score--
+            document.querySelector(".score").textContent = score
+        } else {
+            document.querySelector(".message").textContent = "🥺 You Lose!"
+        }
     }
 })
