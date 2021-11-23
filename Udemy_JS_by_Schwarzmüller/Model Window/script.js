33c3185491f66842f7ecf647da82e44d21c78941
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-23 19:56:11 +0200

    Add functions for opening and closing the modal by click.

--- Udemy_JS_by_Schwarzmüller/Model Window/script.js
+++ Udemy_JS_by_Schwarzmüller/Model Window/script.js
@@ -1,11 +1,23 @@
 "use strict"
 
 var modal = document.querySelector(".modal")
-var ovarlay = document.querySelector(".overlay")
+var overlay = document.querySelector(".overlay")
 var buttonCloseModal = document.querySelector(".close-modal")
 var buttonsShowModal = document.querySelectorAll(".show-modal")
 
-for (let index = 0; index < buttonsShowModal.length; index++) {
-    let element = buttonsShowModal[index].textContent;
-    console.log(element)
+var openModal = function () {
+    modal.classList.remove("hidden")
+    overlay.classList.remove("hidden")
 }
+
+var closeModal = function () {
+    modal.classList.add("hidden")
+    overlay.classList.add("hidden")
+}
+
+for (let index = 0; index < buttonsShowModal.length; index++) {
+    buttonsShowModal[index].addEventListener("click", openModal);
+}
+
+buttonCloseModal.addEventListener("click", closeModal)
+overlay.addEventListener("click", closeModal)
