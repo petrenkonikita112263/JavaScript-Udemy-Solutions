15318272788ab28da9c1116dd2f26882ce437148
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-23 14:52:17 +0200

    Change background color and the width of the number when the user is won.

--- Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
+++ Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
@@ -20,6 +20,8 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector(".message").textContent = "❌ No Number"
     } else if (guess === secretNumber) {
         document.querySelector(".message").textContent = "🎉 Correct Number"
+        document.querySelector("body").style.backgroundColor = "#60b347"
+        document.querySelector(".number").style.width = "30rem"
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "📈 Too High!"
