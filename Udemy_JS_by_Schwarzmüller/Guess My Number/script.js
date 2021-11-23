6bc77e7956b9c7794c5164f3d552033fb07c87cd
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-23 15:18:30 +0200

    Add highscore logic.

--- Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
+++ Udemy_JS_by_Schwarzmüller/Guess My Number/script.js
@@ -11,6 +11,7 @@
 
 var secretNumber = Math.trunc(Math.random() * 20) + 1
 var score = 20
+var highScore = 0
 
 document.querySelector(".check").addEventListener("click", function () {
     let guess = Number(document.querySelector(".guess").value)
@@ -21,6 +22,10 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector(".message").textContent = "🎉 Correct Number"
         document.querySelector("body").style.backgroundColor = "#60b347"
         document.querySelector(".number").style.width = "30rem"
+        if (score > highScore) {
+            highScore = score
+            document.querySelector(".highscore").textContent = highScore
+        }
     } else if (guess > secretNumber) {
         if (score >= 1) {
             document.querySelector(".message").textContent = "📈 Too High!"
