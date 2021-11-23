7aeb4bc39fc95541114f9a6c444bad2531cada8b
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-11-23 17:19:23 +0200

    Select all neccessery elements from the page.

--- Udemy_JS_by_Schwarzmüller/Model Window/script.js
+++ Udemy_JS_by_Schwarzmüller/Model Window/script.js
@@ -1,1 +1,11 @@
 "use strict"
+
+var modal = document.querySelector(".modal")
+var ovarlay = document.querySelector(".overlay")
+var buttonCloseModal = document.querySelector(".close-modal")
+var buttonsShowModal = document.querySelectorAll(".show-modal")
+
+for (let index = 0; index < buttonsShowModal.length; index++) {
+    let element = buttonsShowModal[index].textContent;
+    console.log(element)
+}
