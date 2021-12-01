a7bd8d9c7988e435abb7189eb6938f3f9849b3a9
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-12-01 18:35:14 +0200

    Add function inside function and call it.

--- Udemy_JS_by_Schwarzmüller/Behind Scenes/script.js
+++ Udemy_JS_by_Schwarzmüller/Behind Scenes/script.js
@@ -2,6 +2,12 @@
 
 function calcAge(birthYear) {
     let age = 2037 - birthYear
+
+    function printAge() {
+        let output = `You're ${age}, born in ${birthYear}`
+        console.log(output)
+    }
+    printAge()
     return age
 }
 
