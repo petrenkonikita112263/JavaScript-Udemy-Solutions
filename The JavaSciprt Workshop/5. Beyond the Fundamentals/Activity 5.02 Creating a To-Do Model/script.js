7252c9d4f57f15408bbbe24825e54c65ed083472
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-12-22 19:27:20 +0200

    Add start file for Activity 5.02.

---
+++ The JavaSciprt Workshop/5. Beyond the Fundamentals/Activity 5.02 Creating a To-Do Model/script.js
@@ -0,0 +1,16 @@
+/*
+Let's utilize all the information we have learned about in this chapter and see what we
+have retained. For this activity, imagine that you are working with a team of developers
+and your project is to create a stateful model that stores entries for a To-Do application.
+The model will be a primary function, though other functions can be created and used
+by it. The function will need to store one or more entries and will receive "actions" that
+tell the state to change.
+These actions will include the following:
+• Create a new To-Do
+• Remove a To-Do
+• Modify a To-Do
+Actions will be passed to the state with a given action keyword of either CREATE, REMOVE,
+or MODIFY.
+*/
+
+"use strict"
