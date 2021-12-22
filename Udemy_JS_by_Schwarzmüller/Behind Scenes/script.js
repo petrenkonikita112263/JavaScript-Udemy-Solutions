8f77f13fab1b8cc25b3d299b04acd3d1396f4000
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-12-22 14:43:32 +0200

    Add examples of this keyword.

--- Udemy_JS_by_Schwarzmüller/Behind Scenes/script.js
+++ Udemy_JS_by_Schwarzmüller/Behind Scenes/script.js
@@ -54,3 +54,32 @@ var numberProduct = 10
 function deleteShoppingCart() {
     console.log("All products deleted!")
 }
+
+///////////////////////////////////////
+// The this Keyword in Practice
+console.log(this)
+
+const calcAgeArrow = birthYear => {
+    console.log(2037 - birthYear)
+    console.log(this)
+}
+calcAgeArrow(1980)
+
+const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(this)
+        console.log(2037 - this.year)
+    },
+}
+jonas.calcAge()
+
+const matilda = {
+    year: 2017,
+}
+
+matilda.calcAge = jonas.calcAge
+matilda.calcAge()
+
+const f = jonas.calcAge
+f()
