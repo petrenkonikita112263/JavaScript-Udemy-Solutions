150a76e0a22ab69a7a994b753e68c9ada7f0d530
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-03 13:55:46 +0200

    Complete Coding Challenge #1.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -79,3 +79,26 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 
 GOOD LUCK 😀
 */
+const Car = function (make, speed) {
+    this.make = make
+    this.speed = speed
+}
+
+Car.prototype.accelerate = function () {
+    this.speed += 10
+    console.log(`${this.make} going at ${this.speed} km/h`)
+}
+
+Car.prototype.brake = function () {
+    this.speed -= 5
+    console.log(`${this.make} going at ${this.speed} km/h`)
+}
+
+const car1 = new Car("BMW", 120)
+const car2 = new Car("Mercedes", 95)
+car1.accelerate()
+car1.brake()
+car1.accelerate()
+car2.brake()
+car2.accelerate()
+car2.accelerate()
