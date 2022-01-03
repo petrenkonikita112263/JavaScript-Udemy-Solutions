eff052ee3033d2054d7949c194767feb7ff0f194
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-03 12:16:01 +0200

    Add creation of Objects by Constructor functions.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -1,1 +1,21 @@
 "use strict"
+
+const Person = function (firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+}
+
+// how it works
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const nikita = new Person("Nikita", 1995)
+console.log(nikita)
+
+const matilda = new Person("Matilda", 2017)
+const jack = new Person("Jack", 1975)
+console.log(matilda, jack)
+
+console.log(nikita instanceof Person)
