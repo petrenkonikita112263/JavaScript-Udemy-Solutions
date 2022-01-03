2b8fb615fa73747b048cd3637b6fb7859146263a
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-03 13:03:45 +0200

    Add exasmple of Prototype Inheritance.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -44,3 +44,23 @@ Person.prototype.species = "Homo Sapiens"
 console.log(nikita.species, matilda.species)
 console.log(nikita.hasOwnProperty("firstName"))
 console.log(nikita.hasOwnProperty("species"))
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(nikita.__proto__)
+// Object.prototype (top of prototype chain)
+console.log(nikita.__proto__.__proto__)
+console.log(nikita.__proto__.__proto__.__proto__)
+console.dir(Person.prototype.constructor)
+
+const arr = [3, 6, 4, 5, 6, 9, 3, 9, 6, 3, 4, 5]
+console.log(arr.__proto__)
+console.log(arr.__proto__ === Array.prototype)
+
+Array.prototype.unique = function () {
+    return [...new Set(this)]
+}
+console.log(arr.unique())
+
+const h1 = document.queryCommandValue("h1")
+console.dir(x => x + 1)
