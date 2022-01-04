705d9df0c6e4f7c8b7569821b4bf57f2e16d910d
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-04 12:28:31 +0200

    Add example of creation Classes in ES6.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -102,3 +102,70 @@ car1.accelerate()
 car2.brake()
 car2.accelerate()
 car2.accelerate()
+
+///////////////////////////////////////
+// ES6 Classes
+class PersonClass {
+    constructor(fullName, birthYear) {
+        this.fullName = fullName
+        this.birthYear = birthYear
+    }
+
+    get age() {
+        return 2037 - this.birthYear
+    }
+
+    get fullName() {
+        return this._fullName
+    }
+
+    // Set a property that already exists
+    set fullName(name) {
+        if (name.includes(" ")) {
+            this._fullName = name
+        } else {
+            alert(`${name} is not a full name!`)
+        }
+    }
+
+    // Instance methods
+    // Methods will be added to .prototype property
+    calcAge() {
+        console.log(2037 - this.birthYear)
+    }
+
+    greet() {
+        console.log(`Hey ${this.fullName}`);
+    }
+
+    // Static method
+    static hey() {
+        console.log('Hey there 👋');
+        console.log(this);
+    }
+}
+
+const jessica = new PersonClass("Jessica Davis", 1996)
+console.log(jessica)
+jessica.calcAge()
+console.log(jessica.__proto__ === PersonClass.prototype)
+console.log(jessica.age)
+PersonClass.hey()
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
+
+const account = {
+    owner: "jonas",
+    movements: [200, 530, 120, 300],
+    get latest() {
+        return this.movements.slice(-1).pop()
+    },
+    set latest(mov) {
+        this.movements.push(mov)
+    }
+}
+console.log(account.latest)
+account.latest = 50
+console.log(account.movements)
