028b6ee4e080cfa62571ead8aa429126e84cc3b3
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-05 19:51:49 +0200

    Add example of Inheritance in Constructor Classes.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -240,3 +240,40 @@ console.log(ford.speedUS)
 ford.accelerate()
 ford.brake()
 ford.brake()
+
+///////////////////////////////////////
+// Inheritance Between "Classes": Constructor Functions
+const AnotherPerson = function (firstName, birthYear) {
+    this.firstName = firstName
+    this.birthYear = birthYear
+}
+
+AnotherPerson.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear)
+}
+
+const Student = function (firstName, birthYear, course) {
+    AnotherPerson.call(this, firstName, birthYear)
+    this.course = course
+}
+
+// Linking prototypes
+Student.prototype = Object.create(AnotherPerson.prototype)
+
+Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`)
+}
+
+const mike = new Student("Mike", 2020, "Computer Science")
+mike.introduce()
+mike.calcAge()
+
+console.log(mike.__proto__)
+console.log(mike.__proto__.__proto__)
+
+console.log(mike instanceof Student)
+console.log(mike instanceof AnotherPerson)
+console.log(mike instanceof Object)
+
+Student.prototype.constructor = Student
+console.dir(Student.prototype.constructor)
