922c1580ea0fd6f2212dee4fd35336d1bdb26ab0
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-08 16:27:38 +0200

    Fix console logs.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Arrays and Objects/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Arrays and Objects/script.js
@@ -2,99 +2,106 @@
 
 // Data needed for a later exercise
 const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30"
+    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30"
 
 // Data needed for first part of the section
 const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
+    name: "Classico Italiano",
+    location: "Via Angelo Tavanti 23, Firenze, Italy",
+    categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+    starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+    mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
     },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-  },
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
 
-  orderDelivery: function (starterIndex = 1, mainIndex = 0, time = "20:00", address) {
-    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
-    will be delived to ${address} at ${time}`)
-  }
+    orderDelivery({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = "20:00",
+        address
+    }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+        )
+    },
 }
 
-////////////////////////////
+///////////////////////////////////////
 // Destructuring Objects
 const {
-  name,
-  openingHours,
-  categories
+    name,
+    openingHours,
+    categories
 } = restaurant
 console.log(name, openingHours, categories)
 
 const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags
+    name: restaurantName,
+    openingHours: hours,
+    categories: tags
 } = restaurant
 console.log(restaurantName, hours, tags)
 
 // default values
 const {
-  menu = [], starterMenu: starters = []
+    menu = [], starterMenu: starters = []
 } = restaurant
-console.log(menu, starterMenu)
+console.log(menu, starters)
 
 // mutating variables
-let a = 111
-let b = 999
+let a = 111;
+let b = 999;
 const obj = {
     a: 23,
     b: 7,
     c: 14
-  }
-  ({
+};
+({
     a,
     b
-  } = obj)
-console.log(a, b)
+
+} = obj);
+console.log(a, b);
 
 // nested objects
 const {
-  fri: {
-    open: o,
-    close: c
-  }
+    fri: {
+        open: o,
+        close: c
+    }
 } = openingHours
 console.log(open, close)
 
 restaurant.orderDelivery({
-  time: "22:30",
-  address: "Via del Sale, 21",
-  mainIndex: 2,
-  starterIndex: 2
+    time: "22:30",
+    address: "Via del Sole, 21",
+    mainIndex: 2,
+    starterIndex: 2,
 })
 
 restaurant.orderDelivery({
-  address: "Via del Sale, 21",
-  starterIndex: 2
+    address: "Via del Sole, 21",
+    starterIndex: 1,
 })
 
-////////////////////////////
+///////////////////////////////////////
 // Destructuring Arrays
 const arr = [2, 3, 4]
 const [x, y, z] = arr
@@ -102,7 +109,7 @@ console.log(x, y, z)
 console.log(arr)
 
 let [main, , secondary] = restaurant.categories
-console.log(main, secondary)
+console.log(main, secondary);
 
 // switching variables
 [main, secondary] = [secondary, main]
