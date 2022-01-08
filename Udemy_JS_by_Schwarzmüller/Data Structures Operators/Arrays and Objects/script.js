95ad0f33b067eac468d7753e819f3391ce230404
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-08 12:36:31 +0200

    Add example of destructring of objects.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Arrays and Objects/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Arrays and Objects/script.js
@@ -26,11 +26,76 @@ const restaurant = {
       close: 24,
     },
   },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function (starterIndex = 1, mainIndex = 0, time = "20:00", address) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+    will be delived to ${address} at ${time}`)
   }
 }
 
+////////////////////////////
+// Destructuring Objects
+const {
+  name,
+  openingHours,
+  categories
+} = restaurant
+console.log(name, openingHours, categories)
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags
+} = restaurant
+console.log(restaurantName, hours, tags)
+
+// default values
+const {
+  menu = [], starterMenu: starters = []
+} = restaurant
+console.log(menu, starterMenu)
+
+// mutating variables
+let a = 111
+let b = 999
+const obj = {
+    a: 23,
+    b: 7,
+    c: 14
+  }
+  ({
+    a,
+    b
+  } = obj)
+console.log(a, b)
+
+// nested objects
+const {
+  fri: {
+    open: o,
+    close: c
+  }
+} = openingHours
+console.log(open, close)
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sale, 21",
+  mainIndex: 2,
+  starterIndex: 2
+})
+
+restaurant.orderDelivery({
+  address: "Via del Sale, 21",
+  starterIndex: 2
+})
+
+////////////////////////////
+// Destructuring Arrays
 const arr = [2, 3, 4]
 const [x, y, z] = arr
 console.log(x, y, z)
