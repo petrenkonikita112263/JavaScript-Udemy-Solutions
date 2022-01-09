36c9e14f1bf5a784cfdbc255083ebc571d122875
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-09 12:23:32 +0200

    Create Account class.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -349,3 +349,39 @@ EV.prototype.accelerate = function () {
 const tesla = new EV("Tesla", 100, 0)
 tesla.chargeBattery(22)
 tesla.accelerate()
+
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner
+        this.currency = currency
+        this.pin = pin
+        this.movements = []
+        this.locale = navigator.language
+        console.log(`Thanks for opening an account ${owner}`)
+    }
+
+    deposit(val) {
+        this.movements.push(val)
+    }
+
+    withdraw(val) {
+        this.deposit(-val)
+    }
+
+    approveLoan(val) {
+        return true
+    }
+
+    requestLoan(val) {
+        if (this.approveLoan(val)) {
+            this.deposit(val)
+            console.log("Loan approved")
+        }
+    }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111)
+acc1.deposit(250)
+acc1.withdraw(140)
+acc1.requestLoan(1000)
+console.log(acc1)
