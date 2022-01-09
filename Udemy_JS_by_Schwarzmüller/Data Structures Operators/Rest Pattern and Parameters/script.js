c4564dff6e67d24cdf6f304f605188d6c790c05e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-09 12:13:23 +0200

    Add examples for arrays and objects.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Rest Pattern and Parameters/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Rest Pattern and Parameters/script.js
@@ -41,4 +41,42 @@ const restaurant = {
             `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
         )
     },
+
+    orderPizza: function (mainIngrediant, ...otherIngrediants) {
+        console.log(mainIngrediant, otherIngrediants)
+    }
 }
+
+// Arrays
+const arr = [1, 2, ...[3, 4]]
+const [a, b, ...others] = [1, 2, 3, 4, 5]
+console.log(a, b, others)
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+console.log(pizza, risotto, otherFood)
+
+// Objects
+const {
+    sat,
+    ...weekdays
+} = restaurant.openingHours
+console.log(weekdays)
+
+// Functions
+const add = function (...numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+    console.log(sum)
+}
+
+add(2, 3)
+add(5, 3, 7, 2)
+add(8, 2, 5, 3, 2, 1, 4)
+
+const x = [23, 5, 7]
+add(...x)
+
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach")
+restaurant.orderPizza("mushrooms")
