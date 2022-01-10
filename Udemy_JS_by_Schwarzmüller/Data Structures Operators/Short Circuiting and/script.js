e4c51b30de35557ccd1d4682241b155388b90a44
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-10 12:50:04 +0200

    Add example of ussage of Nullish operator.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
@@ -73,3 +73,13 @@ if (restaurant.orderPasta) {
     restaurant.orderPasta("Mushrooms", "Spinach", "Chees")
 }
 restaurant.orderPasta && restaurant.orderPasta("Mushrooms", "Spinach", "Chees")
+
+///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0
+const guests = restaurant.numGuests || 10
+console.log(guests)
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ? ? 10
+console.log(guestCorrect)
