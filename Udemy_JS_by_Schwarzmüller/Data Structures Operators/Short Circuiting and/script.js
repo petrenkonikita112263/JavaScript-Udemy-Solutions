42e7d88b34137d25d2cf9ca1b3ae7d0b4e8aede0
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-10 12:46:42 +0200

    Add examples of usage || and &&.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
@@ -46,3 +46,30 @@ const restaurant = {
         console.log(ing1, ing2, ing3)
     }
 }
+
+///////////////////////////////////////
+// Short Circuiting (&& and ||)
+console.log("---- OR ----")
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || "Jonas")
+console.log("" || "Jonas")
+console.log(true || 0)
+console.log(undefined || null)
+console.log(undefined || 0 || "" || "Hello" || 23 || null)
+
+restaurant.numGuests = 23
+const quests1 = restaurant.numGuests ? restaurant.numGuests : 19
+console.log(quests1)
+
+const quests2 = restaurant.numGuests || 10
+console.log(quests2)
+
+console.log("---- AND ----")
+console.log(0 && "Jonas")
+console.log(7 && "Jonas")
+console.log("Hello" && 23 && null && "Jonas")
+
+if (restaurant.orderPasta) {
+    restaurant.orderPasta("Mushrooms", "Spinach", "Chees")
+}
+restaurant.orderPasta && restaurant.orderPasta("Mushrooms", "Spinach", "Chees")
