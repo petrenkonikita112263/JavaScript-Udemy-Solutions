99c589e3509246bd5f7ea68c400a880db9db2251
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-11 20:04:11 +0200

    Add complete Challenge #1.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
@@ -1,8 +1,5 @@
 "use strict"
 
-// Data needed for a later exercise
-const flights =
-    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30"
 
 // Data needed for first part of the section
 const restaurant = {
@@ -101,3 +98,81 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
+
+// Data needed for a this exercise
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+    printGoals: function (...playersName) {
+        console.log(playersName)
+        console.log(`${playersName.length} goals were scored`)
+    }
+}
+
+// 1.
+const [players1, players2] = game.players
+console.log(players1, players2)
+
+// 2.
+const [gk, ...fieldPlayers] = players1
+console.log(gk, fieldPlayers)
+
+// 3.
+const allPlayers = [...game.players]
+console.log(allPlayers)
+
+// 4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"]
+console.log(players1Final)
+
+// 5.
+const {
+    team1,
+    x: draw,
+    team2
+} = game.odds
+console.log(team1, draw, team2)
+
+// 6.
+game.printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
+game.printGoals(...game.scored)
+
+// 7.
+game.odds && game.odds.team1 < game.odds.team2 && console.log("Team 1 is more likely to win")
+game.odds && game.odds.team2 < game.odds.team1 && console.log("Team 2 is more likely to win")
