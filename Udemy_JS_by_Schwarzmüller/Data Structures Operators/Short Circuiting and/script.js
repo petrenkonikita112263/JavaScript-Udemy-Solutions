127540e5004bdf6ae440b186071159c97944701d
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-22 19:37:31 +0200

    Fix error.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Short Circuiting and/script.js
@@ -78,7 +78,7 @@ const guests = restaurant.numGuests || 10
 console.log(guests)
 
 // Nullish: null and undefined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ? ? 10
+const guestCorrect = restaurant.numGuests ?? 10
 console.log(guestCorrect)
 
 /* 
