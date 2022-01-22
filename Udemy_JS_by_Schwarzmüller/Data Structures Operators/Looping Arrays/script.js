f2a8a6ec02d2199f5f610a8cb8631bfb27b2f4a4
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-22 19:34:18 +0200

    Add exampled of looping over objects.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Looping Arrays/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Looping Arrays/script.js
@@ -96,3 +96,30 @@ console.log(users[0] ?.name ?? "User array empty")
 
 if (users.length > 0) console.log(users[0].name)
 else console.log("user array empty")
+
+
+///////////////////////////////////////
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours)
+console.log(properties)
+
+let openStr = `We are open on ${properties.length} days: `
+for (const day of properties) {
+  openStr += `${day}, `
+}
+console.log(openStr)
+
+// Property VALUES
+const values = Object.values(openingHours)
+console.log(values)
+
+// Entire object
+const entries = Object.entries(openingHours)
+console.log(entries)
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+    console.log(`On ${day} we open at ${open} and close at ${close}`)
+}
