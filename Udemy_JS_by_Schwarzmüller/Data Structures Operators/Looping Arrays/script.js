628a916d367d047620c09c423daf703b1f766780
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-22 20:08:46 +0200

    Complete Challenge #2.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Looping Arrays/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Looping Arrays/script.js
@@ -147,3 +147,81 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 
 GOOD LUCK 😀
 */
+
+// Data needed for a this exercise
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+    printGoals: function (...playersName) {
+        console.log(playersName)
+        console.log(`${playersName.length} goals were scored`)
+    }
+}
+
+// 1.
+for (const [goal, player] of game.scored.entries()) {
+    console.log(`Goal ${goal}: ${player}`)
+}
+
+// 2.
+var count = 0
+var sumScore = 0
+for (let key in game.odds) {
+    if (game.odds.hasOwnProperty(key)) {
+        sumScore += game.odds[key]
+        count += 1
+    }
+}
+console.log(sumScore/count)
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+    if (game.hasOwnProperty(team)) {
+        console.log(`Odd of victory ${game[team]}: ${game.odds[team]}`)
+    } else {
+        console.log(`Odd of draw: ${game.odds[team]}`)
+    }
+}
+
+// bonus
+const scorers = {}
+for (const player of game.scored) {
+    scorers[player] = scorers[player] ? scorers[player] + 1 : 1
+}
+console.log(scorers)
