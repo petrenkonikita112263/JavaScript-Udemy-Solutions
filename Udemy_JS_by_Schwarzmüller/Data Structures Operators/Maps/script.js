1ab5cb50d7bbfcdb1f51f2d2f3ac075158ea4393
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-23 11:55:34 +0200

    Add more examples of usage Maps.

--- Udemy_JS_by_Schwarzmüller/Data Structures Operators/Maps/script.js
+++ Udemy_JS_by_Schwarzmüller/Data Structures Operators/Maps/script.js
@@ -88,3 +88,36 @@ console.log(rest)
 console.log(rest.size)
 
 console.log(rest.get(arr))
+
+///////////////////////////////////////
+// Maps: Iteration
+const question = new Map([
+    ["question", "What is the best programming language in the world?"],
+    [1, "C"],
+    [2, "Java"],
+    [3, "JavaScript"],
+    ["correct", 3],
+    [true, "Correct 🎉"],
+    [false, "Try again!"],
+  ])
+console.log(question)
+
+// Convert object to map
+console.log(Object.entries(openingHours))
+const hoursMap = new Map(Object.entries(openingHours))
+console.log(hoursMap)
+
+// Quiz app
+console.log(question.get("question"))
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`)
+}
+const answer = 3
+console.log(answer)
+console.log(question.get(question.get("correct") === answer))
+
+// Convert map to array
+console.log([...question])
+// console.log(question.entries())
+console.log([...question.keys()])
+console.log([...question.values()])
