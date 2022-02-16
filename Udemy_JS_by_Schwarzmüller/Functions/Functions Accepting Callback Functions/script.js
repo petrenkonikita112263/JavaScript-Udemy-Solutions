2a6bdb0436b77cae8f707dfdd7d164b0ad763b86
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-02-16 19:34:27 +0200

    Add examples of function that accepts calling other functions.

---
+++ Udemy_JS_by_Schwarzmüller/Functions/Functions Accepting Callback Functions/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const oneWord = function (str) {
+    return str.replace(/ /g, "").toLowerCase()
+}
+
+const upperFirstWord = function (str) {
+    const [firstWord, ...otherWords] = str.split(" ")
+    return [firstWord.toUpperCase(), ...otherWords].join(" ")
+}
+
+// Higher-order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`)
+    console.log(`Transformed string: ${fn(str)}`)
+    console.log(`Transformed by: ${fn.name}`)
+}
+
+transformer("JavaScript is the best!", upperFirstWord)
+transformer("JavaScript is the best!", oneWord)
+
+// JS uses callbacks all the time
+const high5 = function () {
+    console.log("👋")
+}
+document.body.addEventListener("click", high5)
