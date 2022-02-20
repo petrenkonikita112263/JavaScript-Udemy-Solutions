98ef886bc8760b8eaaeee16fa19b804f5a5396a5
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-02-20 22:02:40 +0200

    Add example of function that returns another function.

---
+++ Udemy_JS_by_Schwarzmüller/Functions/Functions Returning Functions/script.js
@@ -0,0 +1,18 @@
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`)
+    }
+}
+
+const greeterHey = greet("Hey")
+greeterHey("Jonas")
+greeterHey("Steven")
+
+greet("Hello")("Jonas")
+
+const greetArrow = (greeting) =>
+    function (name) {
+        console.log(`${greeting} ${name}`)
+    }
+
+greetArrow("Hello")("Jonas")
