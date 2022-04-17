be46076bc8d60dff5508609a4374a4f6a437048f
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-04-17 13:33:24 +0300

    Restructure OOP section.

--- Udemy_JS_by_Schwarzmüller/OOP/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/script.js
@@ -66,44 +66,6 @@ const h1 = document.queryCommandValue("h1")
 console.dir((x) => x + 1)
 
 ///////////////////////////////////////
-// Coding Challenge #1
-
-/* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
-
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
-
-GOOD LUCK 😀
-*/
-const Car = function (make, speed) {
-    this.make = make
-    this.speed = speed
-}
-
-Car.prototype.accelerate = function () {
-    this.speed += 10
-    console.log(`${this.make} going at ${this.speed} km/h`)
-}
-
-Car.prototype.brake = function () {
-    this.speed -= 5
-    console.log(`${this.make} going at ${this.speed} km/h`)
-}
-
-const car1 = new Car("BMW", 120)
-const car2 = new Car("Mercedes", 95)
-car1.accelerate()
-car1.brake()
-car1.accelerate()
-car2.brake()
-car2.accelerate()
-car2.accelerate()
-
-///////////////////////////////////////
 // ES6 Classes
 class PersonClass {
     constructor(fullName, birthYear) {
@@ -194,54 +156,6 @@ sarah.init("Sarah", 1979)
 sarah.calcAge()
 
 ///////////////////////////////////////
-// Coding Challenge #2
-
-/* 
-1. Re-create challenge 1, but this time using an ES6 class;
-2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
-3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
-4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
-
-DATA CAR 1: 'Ford' going at 120 km/h
-
-GOOD LUCK 😀
-*/
-
-class CarClass {
-    constructor(make, speed) {
-        this.make = make
-        this.speed = speed
-    }
-
-    get speedUS() {
-        return this.speed / 1.6
-    }
-
-    set speedUS(speedValue) {
-        this.speed = speedValue * 1.6
-    }
-
-    accelerate() {
-        this.speed += 10
-        console.log(`${this.make} going at ${this.speed} km/h`)
-    }
-
-    brake() {
-        this.speed -= 5
-        console.log(`${this.make} going at ${this.speed} km/h`)
-    }
-}
-
-const ford = new CarClass("Ford", 120)
-console.log(ford.speedUS)
-ford.speedUS = 25
-console.log(ford)
-console.log(ford.speedUS)
-ford.accelerate()
-ford.brake()
-ford.brake()
-
-///////////////////////////////////////
 // Inheritance Between "Classes": Constructor Functions
 const AnotherPerson = function (firstName, birthYear) {
     this.firstName = firstName
@@ -319,40 +233,6 @@ const jay = Object.create(StudentPrototype)
 jay.init("Jay", 2010, "Computer Science")
 jay.introduce()
 jay.calcAge()
-
-///////////////////////////////////////
-// Coding Challenge #3
-
-/* 
-1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
-2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
-3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
-4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
-
-DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
-
-GOOD LUCK 😀
-*/
-const EV = function (make, speed, charge) {
-    Car.call(this, make, speed)
-    this.charge = charge
-}
-
-EV.prototype = Object.create(Car.prototype)
-EV.prototype.chargeBattery = function (chargeTo) {
-    this.charge = chargeTo
-}
-EV.prototype.accelerate = function () {
-    this.speed += 20
-    this.charge -= 1
-    console.log(
-        `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
-    )
-}
-
-const tesla = new EV("Tesla", 100, 0)
-tesla.chargeBattery(22)
-tesla.accelerate()
 
 class Account {
     #movements = []
