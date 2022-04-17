223f5f7455434d6d6bf4b9bfbd28f1585f26ab05
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-04-17 13:45:21 +0300

    Complete Challenge #4.

--- Udemy_JS_by_Schwarzmüller/OOP/Challanges/Challenge #4/script.js
+++ Udemy_JS_by_Schwarzmüller/OOP/Challanges/Challenge #4/script.js
@@ -36,3 +36,39 @@ class CarCl {
         this.speed = speed * 1.6
     }
 }
+
+class EVCl extends CarCl {
+    #charge
+
+    constructor(make, speed, charge) {
+        super(make, speed)
+        this.#charge = charge
+    }
+
+    accelerate() {
+        this.speed += 20
+        this.#charge -= 1
+        console.log(
+            `${this.make} going at ${this.speed} km/h, with a charge of ${
+                this.#charge
+            }%`
+        )
+        return this
+    }
+
+    chargeBattery(chargeTo) {
+        this.#charge = chargeTo
+        return this
+    }
+}
+
+const rivian = new EVCl("Rivian", 120, 23)
+console.log(rivian)
+rivian
+    .accelerate()
+    .accelerate()
+    .accelerate()
+    .brake()
+    .chargeBattery(50)
+    .accelerate()
+console.log(rivian.speedUS)
