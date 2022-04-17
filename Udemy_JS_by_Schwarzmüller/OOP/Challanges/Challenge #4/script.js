cbbf4568bef46ec42d089ca00568eec39ef2eb9e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-04-17 13:35:27 +0300

    Add task for Challenge #4.

---
+++ Udemy_JS_by_Schwarzmüller/OOP/Challanges/Challenge #4/script.js
@@ -0,0 +1,38 @@
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make
+        this.speed = speed
+    }
+
+    accelerate() {
+        this.speed += 10
+        console.log(`${this.make} is going at ${this.speed} km/h`)
+    }
+
+    brake() {
+        this.speed -= 5
+        console.log(`${this.make} is going at ${this.speed} km/h`)
+        return this
+    }
+
+    get speedUS() {
+        return this.speed / 1.6
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6
+    }
+}
