0803a754df66765316b0beba82043303ee5b0227
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-04-21 21:47:23 +0300

    Add examples of looping's methods.

--- Udemy_JS_by_Schwarzmüller/Arrays Bankist/starter/script.js
+++ Udemy_JS_by_Schwarzmüller/Arrays Bankist/starter/script.js
@@ -106,3 +106,49 @@ console.log([...arr, ...arr2])
 
 // JOIN
 console.log(letters.join(" - "))
+
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movementsExample = [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+// for (const movement of movements) {
+for (const [i, movement] of movementsExample.entries()) {
+    if (movement > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${movement}`)
+    } else {
+        console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+    }
+}
+
+console.log("---- FOREACH ----")
+movements.forEach(function (mov, i, arr) {
+    if (mov > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${mov}`)
+    } else {
+        console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+    }
+})
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+
+///////////////////////////////////////
+// forEach With Maps and Sets
+// Map
+const currenciesExample = new Map([
+    ["USD", "United States dollar"],
+    ["EUR", "Euro"],
+    ["GBP", "Pound sterling"],
+])
+
+currenciesExample.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`)
+})
+
+// Set
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"])
+console.log(currenciesUnique)
+currenciesUnique.forEach(function (value, _, map) {
+    console.log(`${value}: ${value}`)
+})
