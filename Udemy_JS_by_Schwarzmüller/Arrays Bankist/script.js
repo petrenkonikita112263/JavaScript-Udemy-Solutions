dfa6b14a2da93094ffcc45b677839d28a4b8d36b
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-04-25 19:21:57 +0300

    Separate Arrays Bankist project and examples of Array's Methods.

---
+++ Udemy_JS_by_Schwarzmüller/Arrays Bankist/script.js
@@ -0,0 +1,191 @@
+"use strict"
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+    owner: "Jonas Schmedtmann",
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+}
+
+const account2 = {
+    owner: "Jessica Davis",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+}
+
+const account3 = {
+    owner: "Steven Thomas Williams",
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+}
+
+const account4 = {
+    owner: "Sarah Smith",
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+}
+
+const accounts = [account1, account2, account3, account4]
+
+// Elements
+const labelWelcome = document.querySelector(".welcome")
+const labelDate = document.querySelector(".date")
+const labelBalance = document.querySelector(".balance__value")
+const labelSumIn = document.querySelector(".summary__value--in")
+const labelSumOut = document.querySelector(".summary__value--out")
+const labelSumInterest = document.querySelector(".summary__value--interest")
+const labelTimer = document.querySelector(".timer")
+
+const containerApp = document.querySelector(".app")
+const containerMovements = document.querySelector(".movements")
+
+const btnLogin = document.querySelector(".login__btn")
+const btnTransfer = document.querySelector(".form__btn--transfer")
+const btnLoan = document.querySelector(".form__btn--loan")
+const btnClose = document.querySelector(".form__btn--close")
+const btnSort = document.querySelector(".btn--sort")
+
+const inputLoginUsername = document.querySelector(".login__input--user")
+const inputLoginPin = document.querySelector(".login__input--pin")
+const inputTransferTo = document.querySelector(".form__input--to")
+const inputTransferAmount = document.querySelector(".form__input--amount")
+const inputLoanAmount = document.querySelector(".form__input--loan-amount")
+const inputCloseUsername = document.querySelector(".form__input--user")
+const inputClosePin = document.querySelector(".form__input--pin")
+
+const displayMovements = function (movements, sort = false) {
+    containerMovements.innerHTML = ""
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements
+    movs.forEach(function (mov, i) {
+        const type = mov > 0 ? "deposit" : "withdrawal"
+        const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+            i + 1
+        } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+      `
+        containerMovements.insertAdjacentHTML("afterbegin", html)
+    })
+}
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+    ["USD", "United States dollar"],
+    ["EUR", "Euro"],
+    ["GBP", "Pound sterling"],
+])
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+/////////////////////////////////////////////////
+// Simple Array Methods
+let arr = ["a", "b", "c", "d", "e"]
+
+// SLICE
+console.log(arr.slice(2))
+console.log(arr.slice(2, 4))
+console.log(arr.slice(-2))
+console.log(arr.slice(-1))
+console.log(arr.slice(1, -2))
+console.log(arr.slice()) // shallow copy
+console.log([...arr]) // shallow copy
+
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1)
+console.log(arr)
+arr.splice(1, 2)
+console.log(arr)
+
+// REVERSE
+arr = ["a", "b", "c", "d", "e"]
+const arr2 = ["j", "i", "h", "g", "f"]
+console.log(arr2.reverse())
+console.log(arr2)
+
+// CONCAT
+const letters = arr.concat(arr2)
+console.log(letters)
+console.log([...arr, ...arr2])
+
+// JOIN
+console.log(letters.join(" - "))
+
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movementsExample = [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+// for (const movement of movements) {
+for (const [i, movement] of movementsExample.entries()) {
+    if (movement > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${movement}`)
+    } else {
+        console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+    }
+}
+
+console.log("---- FOREACH ----")
+movements.forEach(function (mov, i, arr) {
+    if (mov > 0) {
+        console.log(`Movement ${i + 1}: You deposited ${mov}`)
+    } else {
+        console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+    }
+})
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+
+///////////////////////////////////////
+// forEach With Maps and Sets
+// Map
+const currenciesExample = new Map([
+    ["USD", "United States dollar"],
+    ["EUR", "Euro"],
+    ["GBP", "Pound sterling"],
+])
+
+currenciesExample.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`)
+})
+
+// Set
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"])
+console.log(currenciesUnique)
+currenciesUnique.forEach(function (value, _, map) {
+    console.log(`${value}: ${value}`)
+})
+
+///////////////////////////////////////
+// The map Method
+const eurToUsd = 1.1
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movementsExample.map((mov) => mov * eurToUsd)
+console.log(movementsExample)
+console.log(movementsUSD)
+
+const movementsDescriptions = movementsExample.map(
+    (mov, i) =>
+        `Movement ${i + 1}: You ${
+            mov > 0 ? "deposited" : "withdrew"
+        } ${Math.abs(mov)}`
+)
+console.log(movementsDescriptions)
