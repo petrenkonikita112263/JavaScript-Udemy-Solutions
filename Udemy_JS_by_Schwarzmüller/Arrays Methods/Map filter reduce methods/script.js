d254c88c5e97c46dc165eec07ef3fa13f2c860e8
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-04-28 16:33:37 +0300

    Add example of usage reduce method.

--- Udemy_JS_by_Schwarzmüller/Arrays Methods/Map filter reduce methods/script.js
+++ Udemy_JS_by_Schwarzmüller/Arrays Methods/Map filter reduce methods/script.js
@@ -36,3 +36,26 @@ console.log(depositsFor)
 
 const withdrawals = movementsExample.filter((mov) => mov < 0)
 console.log(withdrawals)
+
+///////////////////////////////////////
+// The reduce Method
+console.log(movementsExample)
+
+// accumulator -> SNOWBALL
+// const balance = movementsExample.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movementsExample.reduce((acc, cur) => acc + cur, 0)
+console.log(balance)
+
+let balance2 = 0
+for (const mov of movementsExample) balance2 += mov
+console.log(balance2)
+
+// Maximum value
+const max = movementsExample.reduce((acc, mov) => {
+    if (acc > mov) return acc
+    else return mov
+}, movementsExample[0])
+console.log(max)
