798082f6e82658eb86b1206f5c5e8c38b23204e6
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-05-11 15:50:06 +0300

    Create new section and add examples of converting and checking numbers.

---
+++ Udemy_JS_by_Schwarzmüller/Numbers Dates Timers/script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+console.log(23 == 23.0)
+console.log(0.1 + 0.3 === 0.4)
+
+// converting
+console.log(Number("23"))
+console.log(+"23")
+
+// parsing
+console.log(Number.parseInt("30px"))
+console.log(Number.parseInt("em30"))
+console.log(Number.parseInt("30px", 2))
+console.log(Number.parseInt("2.5rem"))
+console.log(Number.parseFloat("   2.5rem   "))
+
+console.log(Number.isNaN(20))
+console.log(Number.isNaN("20"))
+console.log(Number.isNaN(+"20px"))
+console.log(Number.isNaN(23 / 0))
+
+// checking if the value is a number
+console.log(Number.isFinite(23 / 0))
