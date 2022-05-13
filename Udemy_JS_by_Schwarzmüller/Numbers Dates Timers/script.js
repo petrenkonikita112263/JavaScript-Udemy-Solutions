c0b5c4b2c47f4512939a66aa714ca8c1f6bf0997
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-05-13 10:14:58 +0300

    Add examples of creating and usage of dates.

--- Udemy_JS_by_Schwarzmüller/Numbers Dates Timers/script.js
+++ Udemy_JS_by_Schwarzmüller/Numbers Dates Timers/script.js
@@ -115,3 +115,33 @@ console.log(typeof 20n)
 console.log(20n == "20")
 
 console.log(huge + " is REALLY big!!!")
+
+///////////////////////////////////////
+// Creating Dates
+
+const now = new Date()
+console.log(now)
+console.log(new Date("December 31, 2022"))
+console.log(new Date(2037, 10, 19, 15, 23, 5))
+
+console.log(new Date(0))
+console.log(new Date(3 * 24 * 60 * 60 * 1000))
+
+const future = new Date(2037, 10, 19, 15, 23)
+console.log(future)
+console.log(future.getFullYear())
+console.log(future.getMonth())
+console.log(future.getDate())
+console.log(future.getDay())
+console.log(future.getHours())
+console.log(future.getMinutes())
+console.log(future.getSeconds())
+console.log(future.toISOString())
+console.log(future.getTime())
+
+console.log(new Date(2142256980000))
+
+console.log(Date.now())
+
+future.setFullYear(2040)
+console.log(future)
