41e62b971f55a69284163dbca8a89e9f1f2f9ebc
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-05-23 19:05:50 +0300

    Add events that change background color of nav bar menu.

--- Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
+++ Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
@@ -36,3 +36,30 @@ const firstSection = document.querySelector("#section--1")
 buttonScrollTo.addEventListener("click", function (e) {
     firstSection.scrollIntoView({ behavior: "smooth" })
 })
+
+const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min)
+const randomColor = () =>
+    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
+
+document.querySelector(".nav__link").addEventListener(
+    "click", function (e) {
+        this.style.backgroundColor = randomColor()
+        console.log('LINK', e.target, e.currentTarget)
+        console.log(e.currentTarget === this)
+    }
+)
+
+document.querySelector(".nav__links").addEventListener(
+    "click", function (e) {
+        this.style.backgroundColor = randomColor()
+        console.log('CONTAINER', e.target, e.currentTarget)
+    }
+)
+
+document.querySelector(".nav").addEventListener(
+    "click", function (e) {
+        this.style.backgroundColor = randomColor()
+        console.log('NAV', e.target, e.currentTarget)
+    }
+)
