df4ef8581ab66b6630008d96081a7695ad158bb5
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-05-24 11:42:49 +0300

    Implement smooth page navigation from nav bar.

--- Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
+++ Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
@@ -37,29 +37,39 @@ buttonScrollTo.addEventListener("click", function (e) {
     firstSection.scrollIntoView({ behavior: "smooth" })
 })
 
-const randomInt = (min, max) =>
-    Math.floor(Math.random() * (max - min + 1) + min)
-const randomColor = () =>
-    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
+//const randomInt = (min, max) =>
+//    Math.floor(Math.random() * (max - min + 1) + min)
+//const randomColor = () =>
+//    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
 
-document.querySelector(".nav__link").addEventListener(
-    "click", function (e) {
-        this.style.backgroundColor = randomColor()
-        console.log('LINK', e.target, e.currentTarget)
-        console.log(e.currentTarget === this)
-    }
-)
+//document.querySelector(".nav__link").addEventListener(
+//    "click", function (e) {
+//        this.style.backgroundColor = randomColor()
+//        console.log('LINK', e.target, e.currentTarget)
+//        console.log(e.currentTarget === this)
+//    }
+//)
 
-document.querySelector(".nav__links").addEventListener(
-    "click", function (e) {
-        this.style.backgroundColor = randomColor()
-        console.log('CONTAINER', e.target, e.currentTarget)
-    }
-)
+//document.querySelector(".nav__links").addEventListener(
+//    "click", function (e) {
+//        this.style.backgroundColor = randomColor()
+//        console.log('CONTAINER', e.target, e.currentTarget)
+//    }
+//)
 
-document.querySelector(".nav").addEventListener(
-    "click", function (e) {
-        this.style.backgroundColor = randomColor()
-        console.log('NAV', e.target, e.currentTarget)
+//document.querySelector(".nav").addEventListener(
+//    "click", function (e) {
+//        this.style.backgroundColor = randomColor()
+//        console.log('NAV', e.target, e.currentTarget)
+//    }
+//)
+
+/*page navigation*/
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+    e.preventDefault()
+    if (e.target.classList.contains("nav__link")) {
+        const id = e.target.getAttribute("href")
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" })
     }
-)
+})
+
