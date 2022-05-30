5e63758853b2372c9d63e942672fa2f64bc6d055
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-05-30 17:50:26 +0300

    Implement sticky nav menu.

--- Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
+++ Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
@@ -104,5 +104,23 @@ const handleHover = function (e) {
     }
 }
 
-nav.addEventListener("mouseover", handleHover.bind(0.5));
-nav.addEventListener("mouseout", handleHover.bind(1));
+nav.addEventListener("mouseover", handleHover.bind(0.5))
+nav.addEventListener("mouseout", handleHover.bind(1))
+
+//stick nav menu
+const header = document.querySelector(".header")
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = function (entries) {
+    const [entry] = entries
+    if (!entry.isIntersecting) nav.classList.add("sticky")
+    else nav.classList.remove("sticky")
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+})
+
+headerObserver.observe(header)
