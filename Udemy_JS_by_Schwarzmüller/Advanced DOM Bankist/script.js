2db8949d403795a865992c2aa3073dbb2e55d2ca
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-04 18:17:34 +0300

    Enable lazy loading images.

--- Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
+++ Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
@@ -144,3 +144,22 @@ allSections.forEach(function (section) {
     sectionObserver.observe(section)
     section.classList.add("section--hidden")
 })
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]")
+const loadImg = function (entries, observer) {
+    const [entry] = entries
+    if (!entry.isIntersecting) return
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener("load", function () {
+        entry.target.classList.remove("lazy-img")
+    })
+    observer.unobserve(entry.target)
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: "200px",
+})
+imgTargets.forEach(img => imgObserver.observe(img))
