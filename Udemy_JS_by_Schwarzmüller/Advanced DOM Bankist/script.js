7ddcd1d8335896e4dff92460eac35648e4fbc4b3
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-07 12:50:32 +0300

    Remove commented code.

--- Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
+++ Udemy_JS_by_Schwarzmüller/Advanced DOM Bankist/script.js
@@ -37,33 +37,6 @@ buttonScrollTo.addEventListener("click", function (e) {
     firstSection.scrollIntoView({ behavior: "smooth" })
 })
 
-//const randomInt = (min, max) =>
-//    Math.floor(Math.random() * (max - min + 1) + min)
-//const randomColor = () =>
-//    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
-
-//document.querySelector(".nav__link").addEventListener(
-//    "click", function (e) {
-//        this.style.backgroundColor = randomColor()
-//        console.log("LINK", e.target, e.currentTarget)
-//        console.log(e.currentTarget === this)
-//    }
-//)
-
-//document.querySelector(".nav__links").addEventListener(
-//    "click", function (e) {
-//        this.style.backgroundColor = randomColor()
-//        console.log("CONTAINER", e.target, e.currentTarget)
-//    }
-//)
-
-//document.querySelector(".nav").addEventListener(
-//    "click", function (e) {
-//        this.style.backgroundColor = randomColor()
-//        console.log("NAV", e.target, e.currentTarget)
-//    }
-//)
-
 /*page navigation*/
 document.querySelector(".nav__link").addEventListener("click", function (e) {
     e.preventDefault()
