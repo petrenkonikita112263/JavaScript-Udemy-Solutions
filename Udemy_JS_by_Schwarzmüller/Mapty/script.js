55b433af4b98a0fb88548bd4d6a08d93a5a2d6db
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-11 13:40:34 +0300

    Implement user action with input forms by clicks.

--- Udemy_JS_by_Schwarzmüller/Mapty/script.js
+++ Udemy_JS_by_Schwarzmüller/Mapty/script.js
@@ -11,31 +11,47 @@ const inputDuration = document.querySelector(".form__input--duration")
 const inputCadence = document.querySelector(".form__input--cadence")
 const inputElevation = document.querySelector(".form__input--elevation")
 
+let map, mapEvent
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude } = position.coords
         const { longitude } = position.coords
         const coords = [latitude, longitude]
-        var map = L.map("map").setView(coords, 15)
+        map = L.map("map").setView(coords, 15)
 
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map)
 
-        map.on("click", function (mapEvent) {
-            const {lat, lng} = mapEvent.latlng
-            L.marker([lat, lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: "running-popup"
-                }))
-                .setPopupContent("Workout")
-                .openPopup()
+        map.on("click", function (e) {
+            mapEvent = e
+            form.classList.remove("hidden")
+            inputDistance.focus()
         })
     }, function () {
         alert("Could not get the position")
     })
 }
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = ""
+
+    const { lat, lng } = mapEvent.latlng
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup"
+        }))
+        .setPopupContent("Workout")
+        .openPopup()
+})
+
+inputType.addEventListener("change", function () {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden")
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden")
+})
