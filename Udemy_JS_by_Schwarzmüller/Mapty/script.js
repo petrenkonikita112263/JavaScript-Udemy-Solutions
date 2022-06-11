09322e80c4c37bb156a6f9b408fdf2ec9e752518
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-11 12:12:50 +0300

    Enable usage of leaflet library and add basic code of usage.

--- Udemy_JS_by_Schwarzmüller/Mapty/script.js
+++ Udemy_JS_by_Schwarzmüller/Mapty/script.js
@@ -16,6 +16,16 @@ if (navigator.geolocation) {
         const { latitude } = position.coords
         const { longitude } = position.coords
         console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
+        const coords = [latitude, longitude]
+        var map = L.map("map").setView(coords, 15)
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map)
+
+        L.marker(coords).addTo(map)
+            .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+            .openPopup()
     }, function () {
         alert("Could not get the position")
     })
