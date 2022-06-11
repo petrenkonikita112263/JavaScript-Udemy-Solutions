d92b0510dec0354dae34d278a066be549be199bb
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-11 12:31:20 +0300

    Display new markers with properties by clicking on the map.

--- Udemy_JS_by_Schwarzmüller/Mapty/script.js
+++ Udemy_JS_by_Schwarzmüller/Mapty/script.js
@@ -15,7 +15,6 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude } = position.coords
         const { longitude } = position.coords
-        console.log(`https://www.google.pt/maps/@${latitude},${longitude}`)
         const coords = [latitude, longitude]
         var map = L.map("map").setView(coords, 15)
 
@@ -23,9 +22,19 @@ if (navigator.geolocation) {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map)
 
-        L.marker(coords).addTo(map)
-            .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-            .openPopup()
+        map.on("click", function (mapEvent) {
+            const {lat, lng} = mapEvent.latlng
+            L.marker([lat, lng]).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: "running-popup"
+                }))
+                .setPopupContent("Workout")
+                .openPopup()
+        })
     }, function () {
         alert("Could not get the position")
     })
