2ef1cef04d773e857e7aa81d266a79a8d64fa3a0
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-16 15:11:50 +0300

    Create first AJAX call to retrieve data about Ukraine.

--- Udemy_JS_by_Schwarzmüller/Asynchronous Javascript Block/script.js
+++ Udemy_JS_by_Schwarzmüller/Asynchronous Javascript Block/script.js
@@ -4,3 +4,37 @@ const btn = document.querySelector(".btn-country")
 const countriesContainer = document.querySelector(".countries")
 
 ///////////////////////////////////////
+const getCountryData = function (countryName) {
+    const request = new XMLHttpRequest()
+    request.open(
+        "GET",
+        `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+    )
+    request.send()
+    request.addEventListener("load", function () {
+        console.log(this.responseText)
+        const [data] = JSON.parse(this.responseText)
+        const htmlContent = `
+    <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+                +data.population / 1_000_000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗺</span>${data.area} km2</p>
+            <p class="country__row"><span>🗣️</span>${
+                Object.keys(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+                Object.keys(data.currencies)[0]
+            }</p>
+          </div>
+        </article>
+    `
+        countriesContainer.insertAdjacentHTML("beforeend", htmlContent)
+        countriesContainer.style.opacity = 1
+    })
+}
+getCountryData("Ukraine")
