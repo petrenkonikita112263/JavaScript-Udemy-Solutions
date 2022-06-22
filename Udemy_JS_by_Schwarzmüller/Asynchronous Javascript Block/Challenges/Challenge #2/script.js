e9b2ab16e1b8c2f99b75347b4bc9fa984c7a9470
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-22 10:52:30 +0300

    Complete task for 2nd Challenge.

--- Udemy_JS_by_Schwarzmüller/Asynchronous Javascript Block/Challenges/Challenge #2/script.js
+++ Udemy_JS_by_Schwarzmüller/Asynchronous Javascript Block/Challenges/Challenge #2/script.js
@@ -24,3 +24,56 @@ TEST DATA: Images in the img folder. Test the error handler by passing a wrong i
 
 GOOD LUCK 😀
 */
+
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000)
+    })
+}
+
+const imgContainer = document.querySelector(".images")
+
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement("img")
+        img.src = imgPath
+        img.addEventListener("load", function () {
+            imgContainer.append(img)
+            resolve(img)
+        })
+        img.addEventListener("error", function () {
+            reject(new Error("Image not found"))
+        })
+    })
+}
+
+let currentImg
+
+createImage("img/img-1.jpg")
+    .then((img) => {
+        currentImg = img
+        console.log("Image 1 loaded")
+        return wait(2)
+    })
+    .then(() => {
+        currentImg.style.display = "none"
+        return createImage("img/img-2.jpg")
+    })
+    .then((img) => {
+        currentImg = img
+        console.log("Image 2 loaded")
+        return wait(2)
+    })
+    .then(() => {
+        currentImg.style.display = "none"
+        return createImage("img/img-3.jpg")
+    })
+    .then((img) => {
+        currentImg = img
+        console.log("Image 3 loaded")
+        return wait(2)
+    })
+    .then(() => {
+        currentImg.style.display = "none"
+    })
+    .catch((error) => console.error(error))
