879861158430d16325e7d437e87e6ae4f4097151
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-27 10:39:20 +0300

    Enable npm dependencies. Download lodash-es.

--- Udemy_JS_by_Schwarzmüller/Modern JS Modules Tooling/script.js
+++ Udemy_JS_by_Schwarzmüller/Modern JS Modules Tooling/script.js
@@ -46,3 +46,22 @@ ShoppingCart2.addToCart("bread", 5)
 ShoppingCart2.addToCart("apple", 7)
 console.log(ShoppingCart2)
 console.log(ShoppingCart2.shippingCost)
+
+///////////////////////////////////////
+// Introduction to NPM
+// import cloneDeep from "./node_modules/lodash-es/cloneDeep.js"
+import cloneDeep from "lodash-es"
+const state = {
+    cart: [
+        { product: "bread", quantity: 5 },
+        { product: "pizza", quantity: 5 },
+    ],
+    user: { loggedIn: true },
+}
+const stateClone = Object.assign({}, state)
+const stateDeepClone = cloneDeep(state)
+
+state.user.loggedIn = false
+console.log(stateClone)
+
+console.log(stateDeepClone)
