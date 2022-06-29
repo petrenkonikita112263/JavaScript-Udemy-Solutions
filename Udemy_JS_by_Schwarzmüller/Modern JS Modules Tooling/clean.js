369f7aa823c199ca73c33c438ba1ea3b36120307
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-06-29 11:20:57 +0300

    Fix some bad code.

--- Udemy_JS_by_Schwarzmüller/Modern JS Modules Tooling/clean.js
+++ Udemy_JS_by_Schwarzmüller/Modern JS Modules Tooling/clean.js
@@ -1,64 +1,54 @@
-var budget = [
-  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
-  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
-  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
-  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
-  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
-  { value: -20, description: 'Candy 🍭', user: 'matilda' },
-  { value: -125, description: 'Toys 🚂', user: 'matilda' },
-  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+const budget = [
+    { value: 250, description: "Sold old TV 📺", user: "jonas" },
+    { value: -45, description: "Groceries 🥑", user: "jonas" },
+    { value: 3500, description: "Monthly salary 👩‍💻", user: "jonas" },
+    { value: 300, description: "Freelancing 👩‍💻", user: "jonas" },
+    { value: -1100, description: "New iPhone 📱", user: "jonas" },
+    { value: -20, description: "Candy 🍭", user: "matilda" },
+    { value: -125, description: "Toys 🚂", user: "matilda" },
+    { value: -1800, description: "New Laptop 💻", user: "jonas" },
+]
 
-var limits = {
-  jonas: 1500,
-  matilda: 100,
-};
+const spendingLimits = {
+    jonas: 1500,
+    matilda: 100,
+}
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
+const getLimit = (user) => {
+    spendingLimits[user]?.[user] ?? 0
+}
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
-};
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
-
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
+const addExpense = function (value, description, user = "jonas") {
+    user = user.toLowerCase()
+    if (value <= getLimit(user)) {
+        budget.push({ value: -value, description, user })
     }
+}
+addExpense(10, "Pizza 🍕")
+addExpense(100, "Going to movies 🍿", "Matilda")
+addExpense(200, "Stuff", "Jay")
+console.log(budget)
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
+const checkExpense = function () {
+    for (let entry of budget) {
+        if (entry.value < -getLimit(entry.user)) {
+            entry.flag = "limit"
+        }
     }
-  }
-};
-check();
+}
+checkExpense()
 
-console.log(budget);
+console.log(budget)
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+const logBigExpenses = function (bigLimit) {
+    let output = ""
+    for (let entry of budget) {
+        output +=
+            entry.value <= -bigLimit
+                ? (output += `${entry.description.slice(-2)} / `)
+                : ""
     }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+    output = output.slice(0, -2) // Remove last '/ '
+    console.log(output)
+}
+logBigExpenses(1000)
