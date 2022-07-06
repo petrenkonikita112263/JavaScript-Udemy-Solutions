045c1b0bedfc1373d3a43c8b71ff233f3adfc0bc
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-06 15:43:46 +0300

    Update RecipeView.

--- Udemy_JS_by_Schwarzmüller/Forkify/src/js/views/recipe_view.js
+++ Udemy_JS_by_Schwarzmüller/Forkify/src/js/views/recipe_view.js
@@ -1,25 +1,20 @@
 import icons from "url:../../img/icons.svg"
 import { Fraction } from "fractional"
+import View from "./view.js"
 
-class RecipeView {
-    #parentElement = document.querySelector(".recipe")
-    #data
+class RecipeView extends View {
+    _parentElement = document.querySelector(".recipe")
+    _errorMessage = "We could not find that recipe. Please try another one!"
+    _message = ""
 
-    render(data) {
-        this.#data = data
-        const markupContent = this.#generateMarkup()
-        this.#clear()
-        this.#parentElement.insertAdjacentHTML("afterbegin", markupContent)
-    }
-
-    #generateMarkup() {
+    _generateMarkup() {
         return `
         <figure class="recipe__fig">
-          <img src="${this.#data.image}" alt="${
-            this.#data.title
+          <img src="${this._data.image}" alt="${
+            this._data.title
         }" class="recipe__img" />
           <h1 class="recipe__title">
-            <span>${this.#data.title}</span>
+            <span>${this._data.title}</span>
           </h1>
         </figure>
 
@@ -29,7 +24,7 @@ class RecipeView {
               <use href="${icons}#icon-clock"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--minutes">${
-                this.#data.cooking_time
+                this._data.cooking_time
             }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
@@ -38,7 +33,7 @@ class RecipeView {
               <use href="${icons}#icon-users"></use>
             </svg>
             <span class="recipe__info-data recipe__info-data--people">${
-                this.#data.servings
+                this._data.servings
             }</span>
             <span class="recipe__info-text">servings</span>
 
@@ -71,7 +66,7 @@ class RecipeView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
+          ${this._data.ingredients
               .map((ingredient) => {
                   return `
             <li class="recipe__ingredient">
@@ -99,13 +94,13 @@ class RecipeView {
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
             <span class="recipe__publisher">${
-                this.#data.publisher
+                this._data.publisher
             }</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="${this.#data.sourceUrl}"
+            href="${this._data.sourceUrl}"
             target="_blank"
           >
             <span>Directions</span>
@@ -117,55 +112,11 @@ class RecipeView {
         `
     }
 
-    #clear() {
-        this.#parentElement.innerHTML = ""
-    }
-
-    renderSpinner() {
-        const markupContent = `<div class="spinner">
-    <svg>
-      <use href="${icons}#icon-loader"></use>
-    </svg>
-    </div>`
-        this.#clear()
-        this.#parentElement.insertAdjacentHTML("afterbegin", markupContent)
-    }
-
     addHandlerRender(handler) {
         // prettier-ignore
         ["hashchange", "load"].forEach((elementCommand) => {
             window.addEventListener(elementCommand, handler)
         })
-    }
-
-    renderError(message) {
-        const htmlErrorContent = `
-      <div class="error">
-            <div>
-              <svg>
-                <use href="${icons}#icon-alert-triangle"></use>
-              </svg>
-            </div>
-            <p>${message}</p>
-      </div>
-      `
-        this.#clear()
-        this.#parentElement.insertAdjacentHTML("afterbegin", htmlErrorContent)
-    }
-
-    renderSuccess(message) {
-        const htmlSuccessContent = `
-    <div class="message">
-          <div>
-            <svg>
-              <use href="${icons}#icon-smile"></use>
-            </svg>
-          </div>
-          <p>${message}</p>
-    </div>
-    `
-        this.#clear()
-        this.#parentElement.insertAdjacentHTML("afterbegin", htmlSuccessContent)
     }
 }
 
