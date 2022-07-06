c4d7c1507d86c1e832f56a723088749d340d7c36
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-06 16:00:52 +0300

    Add new method to the model.

--- Udemy_JS_by_Schwarzmüller/Forkify/src/js/model.js
+++ Udemy_JS_by_Schwarzmüller/Forkify/src/js/model.js
@@ -7,6 +7,8 @@ export const state = {
     search: {
         query: "",
         results: [],
+        page: 1,
+        resultsPerPage: 10,
     },
 }
 
@@ -46,4 +48,11 @@ export const loadSearchResults = async function (query) {
     } catch (error) {
         throw error
     }
+}
+
+export const getSearchResultsPage = function (page = state.search.page) {
+    state.search.page = page
+    const start = (page - 1) * state.search.resultsPerPage
+    const end = page * state.search.resultsPerPage
+    return state.search.results.slice(start, end)
 }
