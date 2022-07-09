311a64d27f6d6da5b8f0a8243009641b507ca30e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-09 16:15:49 +0300

    Modify ResultView with one method.

--- Udemy_JS_by_Schwarzmüller/Forkify/src/js/views/results_view.js
+++ Udemy_JS_by_Schwarzmüller/Forkify/src/js/views/results_view.js
@@ -1,9 +1,16 @@
 import View from "./View.js"
+import previewView from "./preview_view.js"
 
 class ResultsView extends View {
     _parentElement = document.querySelector(".results")
     _errorMessage = "No recipes found for your query! Please try again ;)"
     _message = ""
+
+    _generateMarkup() {
+        return this._data
+            .map((result) => previewView.render(result, false))
+            .join("")
+    }
 }
 
 export default new ResultsView()
