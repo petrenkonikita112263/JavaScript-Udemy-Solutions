8c1fe96c4c6979c9ebf767212ae99e2ad80e066e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-10 10:45:01 +0300

    Create new handler function for add bookmark icon.

--- Udemy_JS_by_Schwarzmüller/Forkify/src/js/views/recipe_view.js
+++ Udemy_JS_by_Schwarzmüller/Forkify/src/js/views/recipe_view.js
@@ -60,9 +60,9 @@ class RecipeView extends View {
               <use href="${icons}#icon-user"></use>
             </svg>
           </div>
-          <button class="btn--round">
+          <button class="btn--round btn--bookmark">
             <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
+              <use href="${icons}#icon-bookmark"></use>
             </svg>
           </button>
         </div>
@@ -129,6 +129,14 @@ class RecipeView extends View {
             if (!button) return
             const { updateTo } = button.dataset
             if (+updateTo > 0) handler(+updateTo)
+        })
+    }
+
+    addHandlerAddBookmark(handler) {
+        this._parentElement.addEventListener("click", function (e) {
+            const button = e.target.closest(".btn--bookmark")
+            if (!button) return
+            handler()
         })
     }
 }
