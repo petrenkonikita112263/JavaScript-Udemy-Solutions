3d7a4aa32f16ad12bc179bc3cf1e1e3a5a835805
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-12 19:15:11 +0300

    Fix error and add api key value.

--- Udemy_JS_by_Schwarzmüller/Forkify/src/js/config.js
+++ Udemy_JS_by_Schwarzmüller/Forkify/src/js/config.js
@@ -1,2 +1,3 @@
-const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes"
-const TIMEOUT_SECONDS = +5
+const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/"
+const TIMEOUT_SECONDS = 5
+export const API_KEY = "462b1cc8d4f2730081462fbc65136320"
