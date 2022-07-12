e6b455f25c8c23ce8237c197906298ca83b4d51e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-07-12 19:18:01 +0300

    Create upload method and fix response methods by url link.

--- Udemy_JS_by_Schwarzmüller/Forkify/src/js/model.js
+++ Udemy_JS_by_Schwarzmüller/Forkify/src/js/model.js
@@ -1,6 +1,6 @@
 import { async } from "regenerator-runtime"
-import { API_URL } from "./config.js"
-import { getJsonResponseData } from "./helper.js"
+import { API_URL, API_KEY } from "./config.js"
+import { getJsonResponseData, AJAX } from "./helper.js"
 
 export const state = {
     recipe: {},
@@ -13,20 +13,27 @@ export const state = {
     bookmarks: [],
 }
 
+const createRecipeObject = function (data) {
+    const { recipe } = data.data
+    return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        sourceUrl: recipe.source_url,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients,
+        ...(recipe.key && { key: recipe.key }),
+    }
+}
+
 export const loadRecipe = async function (id) {
     try {
-        const jsonData = await getJsonResponseData(`${API_URL}/${id}`)
-        const { recipe } = jsonData.data
-        state.recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            publisher: recipe.publisher,
-            sourceUrl: recipe.source_url,
-            image: recipe.image_url,
-            servings: recipe.servings,
-            cooking_time: recipe.cooking_time,
-            ingredients: recipe.ingredients,
-        }
+        const data = await AJAX(
+            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key=${API_KEY}`
+        )
+        state.recipe = createRecipeObject(data)
         if (state.bookmarks.some((bookmark) => bookmark.id === id))
             state.recipe.bookmarked = true
         else state.recipe.bookmarked = false
@@ -39,7 +46,9 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
     try {
         state.search.query = query
-        const jsonData = await getJsonResponseData(`${API_URL}?search=${query}`)
+        const jsonData = await AJAX(
+            `https://forkify-api.herokuapp.com/api/v2/recipes/?search=${query}?key=${API_KEY}`
+        )
         state.search.results = jsonData.data.recipes.map((recipe) => {
             return {
                 id: recipe.id,
@@ -95,4 +104,45 @@ init()
 
 const clearBookmarks = function () {
     localStorage.clear("bookmarks")
+}
+
+export const uploadRecipe = async function (newRecipe) {
+    try {
+        const ingredients = Object.entries(newRecipe)
+            .filter(
+                (entry) => entry[0].startsWith("ingredient") && entry[1] !== ""
+            )
+            .map((ingredient) => {
+                const ingredientArr = ingredient[1]
+                    .split(",")
+                    .map((element) => element.trim())
+                if (ingredientArr.length !== 3)
+                    throw new Error(
+                        "Wrong ingredient fromat! Please use the correct format."
+                    )
+                const [quantity, unit, description] = ingredientArr
+                return {
+                    quantity: quantity ? +quantity : null,
+                    unit,
+                    description,
+                }
+            })
+        const recipe = {
+            title: newRecipe.title,
+            source_url: newRecipe.sourceUrl,
+            image_url: newRecipe.image,
+            publisher: newRecipe.publisher,
+            cooking_time: +newRecipe.cookingTime,
+            servings: +newRecipe.servings,
+            ingredients,
+        }
+        const data = await AJAX(
+            `https://forkify-api.herokuapp.com/api/v2/recipes/?key=${API_KEY}`,
+            recipe
+        )
+        state.recipe = createRecipeObject(data)
+        addBookmark(state.recipe)
+    } catch (error) {
+        throw error
+    }
 }
